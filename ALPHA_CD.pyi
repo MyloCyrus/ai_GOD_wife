c3f2f7c3f0e7bbe6f93368d29d408c0830988bf4
Mylo Cyrus <irahiggins740@gmail.com>
2023-09-01 05:56:08 -0700

    Add files via upload

---
+++ ALPHA_CD.pyi
@@ -0,0 +1,4 @@
+< level 1 > = reverse style decoys time layered level
+< level 2 > + obverse style decoys time layered level
+replace line 1                                           [Core]
+[Core]                                                               replace line 2 
